d5fcf562e40c3a4f8a76fc03f2900096218f4900
Nagesh Mishra <nagesh3.13@gmail.com>
2020-07-23 18:00:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:login
+git clone https://github.com/nagesh313/zipkin-service.git
 heroku container:push zipkin --app test-jira-app
+heroku container:release zipkin
+
